d23669ab22055f77a830fe33f529320704273f7c
shin.tada <t1992s0506@gmail.com>
2021-06-11 09:43:11 +0000

    name change

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,19 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+
+    @if (count($errors) > 0 )
+        <ul class="alert alert-danger" role="alert">
+            @foreach ($errors->all() as $error)
+                <li class="ml-4">{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
     
-    <h1>メッセージ新規作成ページ</h1>
+    <h1>タスク新規追加ページ</h1>
     
     <div class="row">
         <div class="col-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
                 <div class="form-group">
-                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
                 
-                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+                {!! Form::submit('追加', ['class' => 'btn btn-primary']) !!}
         
             {!! Form::close() !!}
         </div>
